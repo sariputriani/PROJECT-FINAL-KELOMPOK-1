d7e9e9499fb1d857e65fd17a9aa79d46442b9c47
kissmenasari47@gmail.com <kissmenasari47@gmail.com>
2024-12-28 10:46:24 +0300

    by divan

--- MAHASISWA/mahasiswa.py
+++ MAHASISWA/mahasiswa.py
@@ -11,6 +11,10 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
     QToolBar,
     QSizePolicy,
+    QFrame,
+    QDate,
+    QTableWidget,
+    QCalendarWidget,
     QMessageBox
 )
 from PySide6.QtCore import QSize, Qt
@@ -57,6 +61,104 @@ class HalamanMahasiswa(QMainWindow):
         
         # Mengatur layout untuk widget utama
         self.setLayout(self.layoutMHS)
+
+    def Dashboard(self):
+        container = QWidget()
+        layoutDs = QVBoxLayout()
+        
+        # judul
+        self.judul = QLabel("DASHBOARD")
+        self.judul.setObjectName("lbJudul")        
+        layoutDs.addWidget(self.judul)
+
+        # membuat garis
+        garis = QFrame()
+        garis.setObjectName("line")
+        garis.setFrameShape(QFrame.HLine)
+        garis.setFrameShadow(QFrame.Sunken)
+        layoutDs.addWidget(garis)
+
+        layoutTanggal = QHBoxLayout()
+
+        # Mengambil tanggal hari ini
+        tangglHariIni = QDate.currentDate()
+        # format tanggal
+        format = tangglHariIni.toString("dddd dd - MMMM - yyyy")
+
+        self.tanggal = QLabel(format)
+        self.tanggal.setObjectName("lbTanggal")
+        layoutTanggal.addWidget(self.tanggal)
+
+
+        # Membuat tombol
+        self.btnViewJadwal = QPushButton("  Jadwal")
+        self.btnViewJadwal.setObjectName("btnViewJadwal")
+        self.btnViewJadwal.setFixedSize(90, 30)
+
+        # memberi icon di push button
+        gambarMahasiswa_path = os.path.join(basedir, "gambarMahasiswa", "eyes.png")
+        self.btnViewJadwal.setIcon(QIcon(gambarMahasiswa_path))
+        self.btnViewJadwal.pressed.connect(self.DashboardJadwal)
+        layoutTanggal.addWidget(self.btnViewJadwal)
+        layoutDs.addLayout(layoutTanggal)
+
+        # serch
+        self.search = QLineEdit()
+        self.search.setPlaceholderText("Search Here ...")
+        self.search.setObjectName("search")
+        self.search.textChanged.connect(self.apply_filter)
+        layoutDs.addWidget(self.search)
+
+        # table
+        self.daftarTugas = QTableWidget()
+        self.daftarTugas.setObjectName("daftarTugas")
+        self.daftarTugas.horizontalHeader().setStretchLastSection(True)
+        self.daftarTugas.setColumnCount(8)
+
+        self.daftarTugas.setHorizontalHeaderLabels(["No Tugas","Hari", "Judul Tugas", "Tanggal Pemberian", "Deadline","Waktu","Mata Kuliah","Action"])
+        layoutDs.addWidget(self.daftarTugas)
+        
+        # 
+        layoutDs.addStretch()
+        container.setLayout(layoutDs)
+        self.setCentralWidget(container)
+        self.tugas()
+        # self.pesanPengingat()
+        self.pengingat()
+    
+    def DashboardJadwal(self):
+        container = QWidget()
+        layoutDs = QVBoxLayout()
+        
+        self.judul = QLabel("Jadwal Overview")
+        self.judul.setObjectName("lbJudul")        
+        layoutDs.addWidget(self.judul)
+
+        # membuat garis
+        garis = QFrame()
+        garis.setObjectName("line")
+        garis.setFrameShape(QFrame.HLine)
+        garis.setFrameShadow(QFrame.Sunken)
+        layoutDs.addWidget(garis)
+
+        # celender
+        self.celender = QCalendarWidget()
+        self.celender.setObjectName("tanggal")
+        self.celender.setGridVisible(True)
+        self.current_date = self.celender.selectedDate()  # Ambil tanggal default dari QCalendarWidget
+        layoutDs.addWidget(self.celender)
+
+        self.daftarJadwal = QTableWidget()
+        self.daftarJadwal.setObjectName("daftarJadwal")
+        self.daftarJadwal.horizontalHeader().setStretchLastSection(True)
+        self.daftarJadwal.setColumnCount(6)
+        self.daftarJadwal.setHorizontalHeaderLabels(["Hari","Jam/Waktu", "Nama Ruangan", "Mata Kuliah", "Nama Dosen","SKS"])
+        layoutDs.addWidget(self.daftarJadwal)
+
+        layoutDs.addStretch()
+        container.setLayout(layoutDs)
+        self.setCentralWidget(container)
+        self.jadwal()
 
     def show_langout(self):
         masseg = QMessageBox.question(self, "Konfirmasi", 
