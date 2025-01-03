44553c78700ab5dbe81068ceb33eedd6ae1cbefd
Ismail45z <mael42904@gmail.com>
2025-01-03 14:20:39 +0700

    by el

--- DATABASE/databse.py
+++ DATABASE/databse.py
@@ -2,6 +2,8 @@ import mysql.connector
 
 def buat_koneksi():
     connection = mysql.connector.connect(
+        host = "localhost",
+        port = "3307",
         user = "root",
         password = "",
         database = "tugas_jadwal"
