2b398d8d877d6865f3f858a69a3b2020019ae4f8
sariputriani <kissmenasari47@gmail.com>
2025-01-03 20:23:21 +0300

    by sari

--- DOSEN/dosen.py
+++ DOSEN/dosen.py
@@ -185,6 +185,119 @@ class HalamanDosen(QMainWindow):
          self.showView.show()
 
 
+# class HalamanAddtugas(QWidget):
+    # def __init__(self,parent=None):
+    #     super().__init__()
+    #     self.parent = parent
+    #     self.setWindowTitle("Halaman Tambah Tugas")
+    #     self.setFixedSize(300,350)
+    #     # container = QWidget()
+    #     layout = QVBoxLayout()
+
+    #     # LOGO
+    #     logo = QLabel()
+    #     logoGambar = QPixmap(os.path.join(basedir,"./tugas.png"))
+    #     logo.setPixmap(logoGambar)
+    #     logo.setAlignment(Qt.AlignCenter)
+    #     layout.addWidget(logo)
+
+    #     # ID_TUGAS
+    #     self.layoutidTugas = QHBoxLayout()
+    #     self.llbidTugas = QLabel("No Tugas")
+    #     self.llbidTugas.setObjectName("idtugas")
+    #     self.layoutidTugas.addWidget(self.llbidTugas)
+    #     self.ldidTugas = QLineEdit()
+    #     self.ldidTugas.setObjectName("self.ldidTugas")
+    #     self.ldidTugas.setPlaceholderText("Judul Tugas")
+    #     self.layoutidTugas.addWidget(self.ldidTugas)
+    #     layout.addLayout(self.layoutidTugas)
+
+    #     # ID_MK
+    #     layoutIdMk = QHBoxLayout()
+    #     self.lbMkTugas = QLabel("Id Mata Kuliah")
+    #     self.lbMkTugas.setObjectName("idMk")
+    #     layoutIdMk.addWidget(self.lbMkTugas)
+    #     self.spMk = QComboBox()
+    #     self.spMk.addItems(['NM','PPV'])
+    #     layoutIdMk.addWidget(self.spMk)
+    #     layout.addLayout(layoutIdMk)
+
+    #     # JDL TUGAS
+    #     layoutjdlTugas = QHBoxLayout()
+    #     self.lbjdlTugas = QLabel("No Tugas")
+    #     self.lbjdlTugas.setObjectName("jdltugas")
+    #     layoutjdlTugas.addWidget(self.lbjdlTugas)
+    #     self.ldjdlTugas = QLineEdit()
+    #     self.ldjdlTugas.setObjectName("jdlldtugas")
+    #     self.ldjdlTugas.setPlaceholderText("Judul Tugas")
+    #     layoutjdlTugas.addWidget(self.ldjdlTugas)
+    #     layout.addLayout(layoutjdlTugas)
+    #     # layout.addLayout(layoutIdMk)
+
+    #     # DESKRIPSI TUGAS
+    #     layoutDSTugas = QHBoxLayout()
+    #     self.lbDSTugas = QLabel("Deskripsi tugas")
+    #     self.lbDSTugas.setObjectName("jdltugas")
+    #     layoutDSTugas.addWidget(self.lbDSTugas)
+    #     self.ldDSTugas = QLineEdit()
+    #     self.ldDSTugas.setObjectName("DSldtugas")
+    #     self.ldDSTugas.setPlaceholderText("Deskripsi Tugas")
+    #     layoutDSTugas.addWidget(self.ldDSTugas)
+    #     layout.addLayout(layoutDSTugas)
+
+    #     # TANGGAL PEMBERIAN
+    #     layoutTglMl = QHBoxLayout()
+    #     self.lbtglml = QLabel("Tanggal Pemberian")
+    #     layoutTglMl.addWidget(self.lbtglml)
+    #     self.tglmulai = QDateTimeEdit()
+    #     layoutTglMl.addWidget(self.tglmulai)
+    #     layout.addLayout(layoutTglMl)
+
+    #     # TANGGAL DEADLINE
+    #     layoutTglEnd = QHBoxLayout()
+    #     self.lbtglend = QLabel("Tanggal Pegumpulan")
+    #     layoutTglEnd.addWidget(self.lbtglend)
+    #     self.tglEnd = QDateTimeEdit()
+    #     layoutTglEnd.addWidget(self.tglEnd)
+    #     layout.addLayout(layoutTglEnd)
+
+    #     # btn add
+    #     btnTambah = QPushButton("Tambah")
+    #     layout.addWidget(btnTambah)
+    #     btnTambah.clicked.connect(self.tambah)
+
+    #     # layoutDSTugas.addStretch()
+    #     layout.addStretch()
+
+    #     self.setLayout(layout)
+
+    # def tambah (self):
+    #     idtgs = self.ldidTugas.text()
+    #     idmk = self.spMk.currentText()
+    #     idDsMk = self.ldDSTugas.text()
+    #     idtglml = self.tglmulai.dateTime().toString("yyyy-MM-dd HH:mm:ss")
+    #     idtglend = self.tglEnd.dateTime().toString("yyyy-MM-dd HH:mm:ss")
+
+    #     if not idtgs or not idmk or not idDsMk or not idtglml or not idtglend:
+    #         QMessageBox.warning(self, "Peringatan", "Lengkapi semua data sebelum menambahkan tugas.")
+    #         return
+
+    #     else:    
+    #         conection, curse = buat_koneksi()
+    #         curse = conection.cursor()
+    #         query = ("INSERT INTO tugas (id_tugas, id_mk, deskripsi_tugas, tanggal_pemberian, tanggal_pengumpulan) "
+    #                 "VALUES (%s, %s, %s, %s, %s)")
+    #         ambil = (idtgs, idmk, idDsMk, idtglml, idtglend)
+    #         curse.execute(query, ambil)
+    #         conection.commit()
+    #         QMessageBox.information(self,"Peringatan","Tugas berhasil DItambahkan")
+        
+    #         self.parent.updateDataTableTugas()
+    #         self.ldidTugas.clear()
+    #         self.ldDSTugas.clear()
+    #         self.tglmulai.setDateTime(QDateTime.currentDateTime())
+    #         self.tglEnd.setDateTime(QDateTime.currentDateTime())
+    
 class HalamanAddtugas(QWidget):
     def __init__(self,parent=None):
         super().__init__()
@@ -270,7 +383,8 @@ class HalamanAddtugas(QWidget):
         layout.addStretch()
 
         self.setLayout(layout)
-
+        # container.setLayout(layout)
+    
     def tambah (self):
         idtgs = self.ldidTugas.text()
         idmk = self.spMk.currentText()
@@ -297,147 +411,6 @@ class HalamanAddtugas(QWidget):
             self.ldDSTugas.clear()
             self.tglmulai.setDateTime(QDateTime.currentDateTime())
             self.tglEnd.setDateTime(QDateTime.currentDateTime())
-    
-    
-# class DataUser(QWidget):
-    def __init__(self,username):
-        super().__init__()
-        self.setWindowTitle("Data User")
-        self.setFixedSize(350,350)
-        self.setStyleSheet("""
-            background-color:rgb(235, 241, 243);
-            font-family:Poppins;
-            font-weight:bold;
-            font-size:15px;
-                           """)
-
-        container = QWidget()
-        layout = QVBoxLayout()
-        
-        lbFoto = QLabel()
-        pixmap = QPixmap(os.path.join(basedir, "./gambarMahasiswa/13.png"))
-        lbFoto.setPixmap(pixmap)
-        
-        lbFoto.setAlignment(Qt.AlignCenter)
-        layout.addWidget(lbFoto)
-
-        # nim
-        self.lb2Nim = QLabel("")
-        self.lb2Nim.setObjectName("nim")
-        
-        # nama
-        self.lb2Nama = QLabel("")
-        self.lb2Nama.setObjectName("nama")
-        
-        # menambahakan jurusan
-        self.lb2Jurusan = QLabel("")
-        self.lb2Jurusan.setObjectName("jurusan")
-
-        # menambahkaan prodi
-        self.lb2Prodi = QLabel("")
-        self.lb2Prodi.setObjectName("prodi")
-
-        # menambahkan semester
-        self.lb2Semester = QLabel("")
-        self.lb2Semester.setObjectName("semester")
-
-        # menambhakn widget kedalam layout 
-        layout.addWidget(self.lb2Nim)
-        layout.addWidget(self.lb2Nama)
-        layout.addWidget(self.lb2Jurusan)
-        layout.addWidget(self.lb2Prodi)
-        layout.addWidget(self.lb2Semester)
-
-        # setlayout (layout) kedalam container
-        container.setLayout(layout)
-
-        layout.addStretch()
-        self.setLayout(layout)
-        self.ambil_dataUser(username)
-    
-    def ambil_dataUser(self,username):
-        connection,curse = buat_koneksi()
-        curse = connection.cursor()
-        query = """
-                SELECT datadosen.nip,datadosen.nama_dosen
-                FROM datadosen 
-                JOIN logindosen 
-                ON datadosen.nip = logindosen.username 
-                WHERE logindosen.username = %s;
-            """
-        
-        curse.execute(query,(username,))
-
-        # menampilkan usrname di consle
-        print(username)
-
-        ambildata = curse.fetchone()
-        if ambildata:
-                nip,nama_dosen = ambildata
-                self.lb2Nim.setText(f'Nim            : {nip}')
-                self.lb2Nama.setText(f'Nama         : {nama_dosen}')
-                # self.lb2Jurusan.setText(f'Jurusan     : {jurusan}')
-                # self.lb2Prodi.setText(f'Prodi          : {prodi}')
-                # self.lb2Semester.setText(f'Semester  : {semester}')
-        else:
-                self.lb2Nim.setText(f"Data Tidak ditemukan")
-                self.lb2Nama.setText("Data Tidak ditemukan")
-                self.lb2Jurusan.setText("Data Tidak ditemukan")
-                self.lb2Prodi.setText("Data Tidak ditemukan")
-                self.lb2Semester.setText("Data Tidak ditemukan")
-
-
-# halaman setting
-# class HalamanSetting(QWidget):
-#     def __init__(self):
-#         super().__init__()
-        
-#         # Set layout
-#         self.layout = QVBoxLayout()
-
-#         # Label untuk Halaman Pengaturan
-#         self.label_title = QLabel("Pengaturan Akun")
-#         self.layout.addWidget(self.label_title)
-
-#         # Input untuk mengganti password
-#         self.label_password = QLabel("Ganti Password:")
-#         self.layout.addWidget(self.label_password)
-        
-#         self.input_password = QLineEdit()
-#         self.input_password.setEchoMode(QLineEdit.Password)  # Untuk menyembunyikan input password
-#         self.layout.addWidget(self.input_password)
-        
-#         # Tombol untuk menyimpan perubahan password
-#         self.btn_save_password = QPushButton("Simpan Password")
-#         self.btn_save_password.clicked.connect(self.ganti_password)
-#         self.layout.addWidget(self.btn_save_password)
-
-#         # Tombol untuk logout
-#         self.btn_logout = QPushButton("Logout")
-#         self.btn_logout.clicked.connect(self.logout)
-#         self.layout.addWidget(self.btn_logout)
-
-#         # Set layout ke widget
-#         self.setLayout(self.layout)
-        
-#     def ganti_password(self):
-#         # Ambil password baru dari input
-#         new_password = self.input_password.text()
-        
-#         # Validasi password (misalnya panjang minimal 6 karakter)
-#         if len(new_password) < 6:
-#             QMessageBox.warning(self, "Peringatan", "Password harus minimal 6 karakter.")
-#         else:
-#             # Simpan password baru (ini hanya contoh, proses sesungguhnya tergantung pada aplikasi Anda)
-#             # Di sini Anda bisa menyimpan password ke database atau file
-#             QMessageBox.information(self, "Sukses", "Password berhasil diganti.")
-#             self.input_password.clear()  # Kosongkan input setelah berhasil
-
-#     def logout(self):
-#         # Proses logout (misalnya kembali ke halaman login atau keluar dari aplikasi)
-#         QMessageBox.information(self, "Logout", "Anda telah logout.")
-#         # Di sini bisa menambahkan kode untuk kembali ke halaman login atau keluar dari aplikasi.
-#         self.close()  # Tutup halaman pengaturany
 
 class HalamanSetting(QWidget):
     def __init__(self,username):
@@ -701,10 +674,6 @@ class HalamanView(QWidget):
     def view(self):
         conncetion,curse = buat_koneksi()
         curse = conncetion.cursor()
-
-        query = """
-                select 
-"""
 
     def close_view(self):
         """Close the view page."""
